7625a4493b39e44e93809df27c9b3a92e813e106
kevin <liujinsong@dana.com>
2016-03-30 11:17:30 +0800

    init repo

---
+++ pineapple/dao_generator/src/main/java/com/generator/PineappleGenerator.java
@@ -0,0 +1,46 @@
+package com.generator;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+import de.greenrobot.daogenerator.ToMany;
+
+public class PineappleGenerator {
+
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(1000, "pineapple.bd.com.pineapple.db");
+
+        //注释内容为范例，生成的时候别打开！
+        // addNote(schema);
+        //addCustomerOrder(schema);
+        //用绝对路径找！
+        new DaoGenerator().generateAll(schema, "app/src/main/java");
+    }
+
+    private static void addNote(Schema schema) {
+        Entity note = schema.addEntity("Note");
+        note.addIdProperty();
+        note.addStringProperty("text").notNull();
+        note.addStringProperty("comment");
+        note.addDateProperty("date");
+    }
+
+    private static void addCustomerOrder(Schema schema) {
+        Entity customer = schema.addEntity("Customer");
+        customer.addIdProperty();
+        customer.addStringProperty("name").notNull();
+
+        Entity order = schema.addEntity("Order");
+        order.setTableName("ORDERS"); // "ORDER" is a reserved keyword
+        order.addIdProperty();
+        Property orderDate = order.addDateProperty("date").getProperty();
+        Property customerId = order.addLongProperty("customerId").notNull().getProperty();
+        order.addToOne(customer, customerId);
+
+        ToMany customerToOrders = customer.addToMany(order, customerId);
+        customerToOrders.setName("orders");
+        customerToOrders.orderAsc(orderDate);
+    }
+
+}

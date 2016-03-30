698168e41577076b9594c02aed3b7764059746c8
kevin <liujinsong@dana.com>
2016-03-30 11:25:23 +0800

    add gitignore

--- pineapple/dao_generator/src/main/java/com/generator/PineappleGenerator.java
+++ pineapple/dao_generator/src/main/java/com/generator/PineappleGenerator.java
@@ -6,6 +6,11 @@ import de.greenrobot.daogenerator.Property;
 import de.greenrobot.daogenerator.Schema;
 import de.greenrobot.daogenerator.ToMany;
 
+/**
+ * 这个dao生成工具，编辑的时候不要删别人写的实体，直接自己增加方法，然后执行，就会在
+ * android db包下生成。
+ * 建议在方法注释上加上自己的英文名
+ */
 public class PineappleGenerator {
 
     public static void main(String[] args) throws Exception {
@@ -18,6 +23,10 @@ public class PineappleGenerator {
         new DaoGenerator().generateAll(schema, "app/src/main/java");
     }
 
+    /**
+     * @author:keivn
+     * @param schema
+     */
     private static void addNote(Schema schema) {
         Entity note = schema.addEntity("Note");
         note.addIdProperty();
@@ -25,7 +34,10 @@ public class PineappleGenerator {
         note.addStringProperty("comment");
         note.addDateProperty("date");
     }
-
+    /**
+     * @author:keivn
+     * @param schema
+     */
     private static void addCustomerOrder(Schema schema) {
         Entity customer = schema.addEntity("Customer");
         customer.addIdProperty();

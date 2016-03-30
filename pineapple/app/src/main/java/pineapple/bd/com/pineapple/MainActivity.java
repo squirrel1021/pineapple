08dc83f5e16b1309c5e71b91e37ea6b0eeffa31b
kevin <liujinsong@dana.com>
2016-03-30 18:33:57 +0800

    remove push

--- pineapple/app/src/main/java/pineapple/bd/com/pineapple/MainActivity.java
+++ pineapple/app/src/main/java/pineapple/bd/com/pineapple/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.logging.Logger;
+
+import cn.bmob.v3.listener.SaveListener;
+import pineapple.bd.com.pineapple.db.User;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +33,32 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        saveUser();
+    }
+
+    private void saveUser() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                User user = new User();
+                user.setNickname("kevin");
+                user.setGender(1);
+                user.setHobbies("虐伯翔！");
+                user.save(MainActivity.this, new SaveListener() {
+                    @Override
+                    public void onSuccess() {
+                        Log.e(TAG,"save user onSuccess");
+                    }
+
+                    @Override
+                    public void onFailure(int i, String s) {
+                        Log.e(TAG,"save user onFailure");
+                    }
+                });
+            }
+        }).start();
+
     }
 
     @Override

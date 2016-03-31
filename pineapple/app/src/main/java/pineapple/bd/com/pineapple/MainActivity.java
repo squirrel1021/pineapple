d075e06cbc39fb804b2cd7ae4d5baf5487c67c8b
alvin <alvin@163.com>
2016-03-31 17:31:29 +0800

    merge

--- pineapple/app/src/main/java/pineapple/bd/com/pineapple/MainActivity.java
+++ pineapple/app/src/main/java/pineapple/bd/com/pineapple/MainActivity.java
@@ -4,28 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
 import android.widget.Button;
 
-import java.io.IOException;
-
-import okhttp3.Headers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
-import org.json.JSONArray;
-
-import java.util.logging.Logger;
-
-import cn.bmob.v3.Bmob;
-import cn.bmob.v3.BmobQuery;
-import cn.bmob.v3.listener.DeleteListener;
-import cn.bmob.v3.listener.FindCallback;
 import cn.bmob.v3.listener.SaveListener;
-import pineapple.bd.com.pineapple.account.AccountService;
-import pineapple.bd.com.pineapple.account.AccountType;
-import pineapple.bd.com.pineapple.db.User;
 import pineapple.bd.com.pineapple.db.UserAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,47 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-<<<<<<< HEAD
-        btn_login = (Button)findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new View.OnClickListener() {
 
-=======
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
->>>>>>> 4251da9e0e405744c8be64b39832e2aada299a5a
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        try{
-                        Request request = new Request.Builder()
-                                .url("http://publicobject.com/helloworld.txt")
-                                .build();
-                        final OkHttpClient client = new OkHttpClient();
-                        Response response = client.newCall(request).execute();
-                        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-
-                        Headers responseHeaders = response.headers();
-                        for (int i = 0; i < responseHeaders.size(); i++) {
-                            System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-                        }
-
-                        System.out.println(response.body().string());
-                        }catch (Exception e){
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
-            }
-        });
-<<<<<<< HEAD
-
-=======
-
-
-        AccountService.getInstance().register(this,AccountType.PINEAPPLE,"kevin","123456");
+        // used on register!
+       // AccountService.getInstance().register(this,AccountType.PINEAPPLE,"kevin","123456");
 
     }
 
@@ -109,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
->>>>>>> 4251da9e0e405744c8be64b39832e2aada299a5a
+
     }
 
 

3a7edc0990a7972a3cffb02ab6887628271a1adf
kevin <liujinsong@dana.com>
2016-04-18 14:23:08 +0800

    add upload

--- pineapple/app/src/main/java/pineapple/bd/com/pineapple/GenerateTablesActivity.java
+++ pineapple/app/src/main/java/pineapple/bd/com/pineapple/GenerateTablesActivity.java
@@ -1,51 +1,121 @@
 package pineapple.bd.com.pineapple;
 
+import android.Manifest;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.suitebuilder.annotation.Suppress;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.datatype.BmobFile;
+import cn.bmob.v3.listener.UploadFileListener;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
 import pineapple.bd.com.pineapple.db.GreenDaoUtils;
 import pineapple.bd.com.pineapple.db.Music;
+import pineapple.bd.com.pineapple.entity.MediaType;
+import pineapple.bd.com.pineapple.http.FileUploader;
+import pineapple.bd.com.pineapple.utils.BasicUtils;
+import pineapple.bd.com.pineapple.utils.FileUtils;
 import pineapple.bd.com.pineapple.utils.logUtils.Logs;
-
+@RuntimePermissions
 public class GenerateTablesActivity extends AppCompatActivity {
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        GenerateTablesActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_tables);
-        //createMusicTable();
+        GenerateTablesActivityPermissionsDispatcher.uploadFileWithCheck(this);
     }
 
-    public void createMusicTable() {
-        try {
+    @NeedsPermission({Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    public void uploadFile() {
 
-            String json = readFromAssets("tables.json", this);
+        String json = readFromAssets("tables.json", this);
+        List<Music> musics = parseList(json, new TypeToken<List<Music>>() {
+        });
+        Logs.e("music:" + json);
 
-            Logs.e("music:" + json);
-            List<Music> musics = parseList(json, new TypeToken<List<Music>>() {
-            });
-           // GreenDaoUtils.insert(GreenDaoUtils.getSession().getMusicDao(), musics);
-            List<Music> ret = GreenDaoUtils.getList(GreenDaoUtils.getSession().getMusicDao());
-
-            for (Music music :
-                    ret) {
-                Logs.e("music:" + music);
+        // GreenDaoUtils.insert(GreenDaoUtils.getSession().getMusicDao(), musics);
+//            List<Music> ret = GreenDaoUtils.getList(GreenDaoUtils.getSession().getMusicDao());
+//
+            for (int i=0;i<musics.size();i++) {
+                upload(posters[i],musics.get(i));
             }
+    }
 
+    String[] posters = {"spring.jpg","love_story.jpg"};
+
+
+    private void upload(final String file,final Music music) {
+        final StringBuffer buffer = new StringBuffer();
+        try {
+            buffer.append(FileUtils.getCurrentDataPath(PineApplication.mContext)).append('/').append(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Callable<Boolean> wirteCallable = new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                    InputStream fIn = getResources().getAssets()
+                            .open(file, Context.MODE_WORLD_READABLE);
+                    FileUtils.copyFileFromInputStream(fIn,buffer.toString());
+                return true;
+            }
+        };
+        Future<Boolean> writerFuture = Executors.newSingleThreadExecutor().submit(wirteCallable);
+        try {
+            if(writerFuture.get()){
+                final FileUploader fileUploader = new FileUploader(buffer.toString(), new FileUploader.UploadListener() {
+                    @Override
+                    public void onSuccess(File file,String url) {
+                        createMusic(music,file.length(),url);
+                    }
+                    @Override
+                    public void onFailure(int i, String s) {
+
+                    }
+                });
+                Executors.newSingleThreadExecutor().submit(fileUploader);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createMusic(Music music,long music_size,String poster_url) {
+        try {
+            music.setPoster_url(poster_url);
+            music.setSize(music_size);
+            music.setMediaType(MediaType.MP3.value);
+            music.save(GenerateTablesActivity.this);
         } catch (Exception e) {
             e.printStackTrace();
             Logs.e(e);
         }
     }
+
 
     public String readFromAssets(String fileName, Context context) {
         StringBuilder returnString = new StringBuilder();
---
+++ pineapple/app/src/main/java/pineapple/bd/com/pineapple/http/FileUploader.java
@@ -0,0 +1,94 @@
+package pineapple.bd.com.pineapple.http;
+
+import android.text.TextUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.concurrent.Callable;
+
+import cn.bmob.v3.datatype.BmobFile;
+import cn.bmob.v3.listener.UploadFileListener;
+import pineapple.bd.com.pineapple.PineApplication;
+import pineapple.bd.com.pineapple.utils.logUtils.Logs;
+
+/**
+ * Description : <Content><br>
+ * CreateTime : 2016/4/18 11:06
+ *
+ * @author KevinLiu
+ * @version <v1.0>
+ * @Editor : KevinLiu
+ * @ModifyTime : 2016/4/18 11:06
+ * @ModifyDescription : <Content>
+ */
+public class FileUploader implements Callable<Boolean> {
+
+    private String mFilePath;
+
+    private File mFile;
+
+    private UploadListener mUploadListener;
+
+    private static UploadListener defaultUploadListener = new UploadListener() {
+        @Override
+        public void onSuccess(File file,String url) {
+
+        }
+
+        @Override
+        public void onFailure(int i, String s) {
+
+        }
+    };
+
+    public FileUploader(String filePath) {
+        this(filePath,defaultUploadListener);
+    }
+
+    public FileUploader(File file) {
+        this(file,defaultUploadListener);
+    }
+
+    public FileUploader(String filePath,UploadListener uploadListener) {
+        this.mFilePath = filePath;
+        this.mUploadListener = uploadListener;
+    }
+
+    public FileUploader(File file,UploadListener uploadListener) {
+        this.mFile = file;
+        this.mUploadListener = uploadListener;
+    }
+
+    @Override
+    public Boolean call() throws Exception {
+
+        if(!TextUtils.isEmpty(mFilePath)){
+            mFile = new File(mFilePath);
+        }
+        if(null==mFile||!mFile.exists()){
+            throw new FileNotFoundException();
+        }
+
+        final BmobFile bmobFile = new BmobFile(mFile);
+        bmobFile.uploadblock(PineApplication.mContext, new UploadFileListener() {
+            @Override
+            public void onSuccess() {
+                Logs.e("bmobFile upload onSuccess: " + bmobFile.getUrl());
+                FileUploader.this.mUploadListener.onSuccess(mFile,bmobFile.getUrl());
+            }
+
+            @Override
+            public void onFailure(int i, String s) {
+                Logs.e("bmobFile upload onFailure: " + s);
+                FileUploader.this.mUploadListener.onFailure(i,s);
+            }
+        });
+
+        return true;
+    }
+
+    public static interface UploadListener{
+        void onSuccess(File file,String url);
+        void onFailure(int i, String s);
+    }
+}
